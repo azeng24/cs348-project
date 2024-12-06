4afa50592032827dde7077a4054c52253b783f99
azeng24 <zengannie81437@gmail.com>
2024-12-06 16:42:26 -0500

    remove hello_world project

--- hello_world/HelloWorld_App/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def sayHello(request):
-    return HttpResponse('<h1 style="color:red">Hello, World!</h1>')
